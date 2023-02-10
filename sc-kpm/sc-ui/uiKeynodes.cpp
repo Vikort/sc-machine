426bb2859afd4f34d15a20cc25db34a5671d4e69
Viktor Markovec <markvik42@gmail.com>
2023-02-10 20:07:55 +0300

    [ui] Add order

--- sc-kpm/sc-ui/uiKeynodes.cpp
+++ sc-kpm/sc-ui/uiKeynodes.cpp
@@ -28,6 +28,8 @@ const char keynode_nrel_translation_str[] = "nrel_translation";
 const char keynode_nrel_format_str[] = "nrel_format";
 const char keynode_nrel_system_identifier_str[] = "nrel_system_identifier";
 const char keynode_nrel_main_idtf_str[] = "nrel_main_idtf";
+const char keynode_nrel_idtf_str[] = "nrel_idtf";
+const char keynode_rrel_key_sc_element_str[] = "rrel_key_sc_element";
 
 const char keynode_command_generate_instance_str[] = "ui_command_generate_instance";
 const char keynode_command_initiated_str[] = "ui_command_initiated";
@@ -59,6 +61,8 @@ sc_addr keynode_nrel_translation;
 sc_addr keynode_nrel_format;
 sc_addr keynode_nrel_system_identifier;
 sc_addr keynode_nrel_main_idtf;
+sc_addr keynode_nrel_idtf;
+sc_addr keynode_rrel_key_sc_element;
 
 sc_addr keynode_command_generate_instance;
 sc_addr keynode_command_initiated;
@@ -95,6 +99,8 @@ sc_bool initialize_keynodes()
   RESOLVE_KEYNODE(s_default_ctx, keynode_nrel_format);
   RESOLVE_KEYNODE(s_default_ctx, keynode_nrel_system_identifier);
   RESOLVE_KEYNODE(s_default_ctx, keynode_nrel_main_idtf);
+  RESOLVE_KEYNODE(s_default_ctx, keynode_nrel_idtf);
+  RESOLVE_KEYNODE(s_default_ctx, keynode_rrel_key_sc_element);
 
   RESOLVE_KEYNODE(s_default_ctx, keynode_command_generate_instance);
   RESOLVE_KEYNODE(s_default_ctx, keynode_command_initiated);
